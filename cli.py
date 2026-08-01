d606b116f27924371b44f1eb8ae6410ab54da283
Chris McKeel <chrisfox9158@gmail.com>
2026-08-01 12:05:03 -0400

    Make --plant argument required and add default values for --steps and --timestep

--- cli.py
+++ cli.py
@@ -3,5 +3,7 @@ from plants import PLANTS
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Plant testbed for pidkit validation against dynamic simulations")
-    parser.add_argument("--plant", choices=PLANTS.keys(), help="Plant simulation model")
+    parser.add_argument("--plant", choices=PLANTS.keys(), required=True, help="Plant simulation model")
+    parser.add_argument("--steps", type=int, default=100, help="Steps run for PID loop")
+    parser.add_argument("--timestep", type=float, default=0.1, help="dt timestep for PID loop")
     return parser.parse_args()
