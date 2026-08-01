7a8b36b637b478ba0afd5757c3bb1c5364b790b6
Chris McKeel <chrisfox9158@gmail.com>
2026-08-01 11:13:24 -0400

    Add CartVelocity to PLANTS registry

--- plants/__init__.py
+++ plants/__init__.py
@@ -1,5 +1,7 @@
 from .thermostat import Thermostat
+from .cart_velocity import CartVelocity
 
 PLANTS = {
-    "thermostat": Thermostat
+    "thermostat": Thermostat,
+    "cart_velocity": CartVelocity
 }
