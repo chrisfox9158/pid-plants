70e514c3bdf788fda008791fe77c737c5b42ad93
Chris McKeel <chrisfox9158@gmail.com>
2026-08-05 00:29:16 -0400

    Add delayed first-order simulation ThermostatDelayed class

--- plants/__init__.py
+++ plants/__init__.py
@@ -1,7 +1,9 @@
 from .thermostat.thermostat import Thermostat
+from.thermostat_delayed.thermostat_delayed import ThermostatDelayed
 from .cart_velocity.cart_velocity import CartVelocity
 
 PLANTS = {
     "thermostat": Thermostat,
+    "thermostat_delayed": ThermostatDelayed,
     "cart_velocity": CartVelocity
 }
---
+++ plants/thermostat_delayed/thermostat_delayed.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+class ThermostatDelayed:
+    PARAMS = {
+        "control_scale": {"prompt": "Control Scale (strength of thermostat effects)", "type": float, "default": 1.0},
+        "response_scale": {"prompt": "Response Scale (high = slow-heating; low = fast-heating)", "type": float, "default": 60.0},
+        "t_ambient": {"prompt": "Ambient (Room) Temperature", "type": float, "default": 21.0},
+        "t_initial": {"prompt": "Initial Temperature", "type": float, "default": None},
+        "dt": {"prompt": "Timestep (should match arg-passed timestep)", "type": float, "default": 1},
+        "theta": {"prompt": "Dead Time (time before control affects system)", "type": float, "default": 5.0},
+    }
+    
+    def __init__(self, control_scale=1.0, response_scale=60.0, t_ambient=21.0, t_initial=None, dt=1, theta=5.0):
+        if response_scale <= 0:
+            raise ValueError("response_scale must be positive.")
+        if t_initial is None:
+            t_initial = t_ambient
+
+        self.k = control_scale
+        self.tau = response_scale
+        self.t_ambient = t_ambient
+        self.t_initial = t_initial
+        self.t_current = self.t_initial
+        self.dt = dt
+        self.theta = theta
+        self.maxlen = int(self.theta / self.dt)
+        self.u_values = deque(maxlen=self.maxlen)
+
+    def step(self, u, dt):
+        if dt != self.dt:
+            raise RuntimeError("step() timestep does not equal plant class timestep; please ensure initial dt matches the passed step() dt value.")
+
+        if len(self.u_values) >= int(self.theta / dt):
+            u_delayed = self.u_values[0]
+        else:
+            u_delayed = 0
+        t_new = self.t_current + ((-(self.t_current - self.t_ambient) + self.k * u_delayed) / self.tau) * dt
+
+        self.t_current = t_new
+        self.u_values.append(u)
+        return self.t_current
+
+    def get_state(self):
+        return self.t_current
+
+    def reset(self):
+        self.t_current = self.t_initial
+        self.u_values = deque(maxlen=self.maxlen)
